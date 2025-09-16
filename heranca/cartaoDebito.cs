fdca6e70928a258d3525d5ded7119cd8608ea65a
Paulo Magalhães <113356681+PaulinhoMDS@users.noreply.github.com>
2025-09-16 00:11:49 +0000

    feat: classe cartao

---
+++ heranca/cartaoDebito.cs
@@ -0,0 +1,20 @@
+public class CartaoDebito
+{
+    public CartaoDebito()
+    {
+        Bandeira = BandeiraCartao.Mastercard;
+    }
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string Cvv { get; set; }
+    
+    }
+}
+
+public enum BandeiraCartao{
+    Visa,
+    Mastercard,
+    Amex,
+    Elo,
+}
